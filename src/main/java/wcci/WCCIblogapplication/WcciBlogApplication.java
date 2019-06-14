efe87eac71a6b647227cbec23dc7ac7283162a1e
matthew-laine <matthew.i.laine@gmail.com>
2019-06-14 14:54:44 -0400

    first commit

---
+++ src/main/java/wcci/WCCIblogapplication/WcciBlogApplication.java
@@ -0,0 +1,13 @@
+package wcci.WCCIblogapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcciBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcciBlogApplication.class, args);
+	}
+
+}
